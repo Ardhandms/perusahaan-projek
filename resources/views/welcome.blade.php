56318758a89281d29115ac220013b05934d1343b
Ardhan Dimas Nuryadin <ardhandimas9@gmail.com>
2023-09-19 22:42:46 +0700

    commit pertama

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,78 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link href="https://cdnjs.cloudflare.com/ajax/libs/flowbite/1.8.1/flowbite.min.css"  rel="stylesheet" />
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/flowbite/1.8.1/flowbite.min.js"></script>
+
+    <style>
+    header {
+        border-bottom: 2px solid #e5e7eb;
+    }
+
+    section {
+        margin-top: 20px; 
+    }
+</style>
+</head>
+<body>
+    <header>
+        <nav>     
+
+            <nav class="border-gray-200 bg-gray-50 dark:bg-gray-800 dark:border-gray-700">
+            <div class="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
+                <a href="#" class="flex items-center">
+                    <img src="https://flowbite.com/docs/images/logo.svg" class="h-8 mr-3" alt="Flowbite Logo" />
+                    <span class="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">PERUSAHAAN</span>
+                </a>
+                <a href="#" class="hidden md:inline-flex items-center ml-auto mr-3 text-sm text-gray-500 hover:text-gray-700 dark:text-gray-400 dark:hover:text-white">Login</a>
+                <button data-collapse-toggle="navbar-hamburger" type="button" class="inline-flex items-center justify-center p-2 w-10 h-10 ml-3 text-sm text-gray-500 rounded-lg hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600" aria-controls="navbar-hamburger" aria-expanded="false">
+                <span class="sr-only">Open main menu</span>
+                <svg class="w-5 h-5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 17 14">
+                    <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M1 1h15M1 7h15M1 13h15"/>
+                </svg>
+                </button>
+                <div class="hidden w-full" id="navbar-hamburger">
+                <ul class="flex flex-col font-medium mt-4 rounded-lg bg-gray-50 dark:bg-gray-800 dark:border-gray-700">
+                    <li>
+                    <a href="#" class="block py-2 pl-3 pr-4 text-white bg-blue-700 rounded dark:bg-blue-600" aria-current="page">Dashboard</a>
+                    </li>
+                    <li>
+                    <a href="#" class="block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white">Services</a>
+                    </li>
+                    <li>
+                    <a href="#" class="block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 dark:text-gray-400 md:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white">Pricing</a>
+                    </li>
+                    <li>
+                    <a href="#" class="block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white">Contact</a>
+                    </li>
+                </ul>
+                </div>
+            </div>
+            </nav>
+
+        </nav>
+    </header>
+    
+    <section>
+        <div class="max-w-screen-xl mx-auto px-4">
+                <h1 class="text-2xl font-semibold mb-4">Kabar Terbaru Perusahaan</h1>
+                <div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
+
+                <div class="bg-white p-4 rounded-lg shadow-md">
+                    <h2 class="text-lg font-semibold mb-2">Kabar Terkini</h2>                  
+                    <img class="h-auto max-w-full" src="{{ asset('images/company.jpg') }}" alt="image description">
+                    <p class="text-gray-600">Lihat apa saja yang terjadi belakangan ini di lingkungan Perusahaan.</p>
+                    <a href="#" class="text-blue-500 hover:underline">Baca selengkapnya</a>
+                </div>
+
+        
+    </section>
+
+    <footer>
+
+    </footer>
+</body>
+</html>
